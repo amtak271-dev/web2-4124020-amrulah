e4dade951f55bc10d7c45fe40fb8635e3bdb59fd
unknown <you@example.com>
2026-02-28 20:18:30 +0700

    Initial commit Laravel Web2

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/perkenalan', function () {
+    return '
+        <h1>Halo! Nama saya Amrulah</h1>
+        <p>NIM: 4124020 | Prodi: Sistem Informasi</p>
+        <p>Saya siap belajar Laravel! 🚀</p>
+    ';
+});
